3521d7ac4d543a0674c634e18cbfa5ee39acfb72
JesseAngeles <angeleslopezerickjesse@gmail.com>
2021-06-08 01:17:05 -0500

    funcional 2

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
-const mysql = require('mysql');
-const CryptoJS = require("crypto-js");
 
 const path = require('path');
 
@@ -14,26 +12,9 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(function(req, res) {
-    res.status(404);
-    // respond with html page
-    if (req.accepts('html')) {
-        res.json('404', {
-            'ERROR 404': 'La pagina que esta buscanda no existe o se pudo encontrar',
-            'ESTADO': 'TuT'
-        });
-        return;
-    }
-
-});
-
-
-//IO = esta es la comunicación del backend
+// IO = esta es la comunicacion del backend
 module.exports.io = socketIO(server);
-require('./sockets/sockets');
-
+require('./sockets/socket');
 server.listen(port, (err) => {
 
     if (err) throw new Error(err);
